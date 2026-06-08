5b14eb67bdb2ea1206a2c5cd74d1fcb2d1635eb9
nitron1108 <mja29122@gmail.com>
2026-06-09 02:40:16 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,41 @@
 
 #include "tree.h"
 
-using namespace std;
 
 int main() {
-    vector<char> symbols = {'1', '2', '3'};
+    std::vector<char> symbols = {'1', '2', '3'};
 
     PMTree tree(symbols);
-    vector<vector<char>> allPerms = getAllPerms(tree);
-    cout << "все перестановки:\n";
+    std::vector<std::vector<char>> allPerms = getAllPerms(tree);
+    std::cout << "все перестановки:\n";
 
-    for (size_t i = 0; i < allPerms.size(), i++) {
-        cout << i + 1 << ":";
+    for (size_t i = 0; i < allPerms.size(); i++) {
+        std::cout << i + 1 << ":";
 
         for (size_t j = 0; j < allPerms[i].size(); j++) {
-            cout << allPerms[i][j];
+            std::cout << allPerms[i][j];
         }
 
-        cout << "\n";
+        std::cout << "\n";
     }
 
     int num = 2;
 
-    vector<char> perm1 = getPerm1(tree, num);
-    vector<char> perm2 = getPerm2(tree, num);
-    cout << "\nперестановка №" << num << "\n";
+    std::vector<char> perm1 = getPerm1(tree, num);
+    std::vector<char> perm2 = getPerm2(tree, num);
+    std::cout << "\nперестановка №" << num << "\n";
 
-    cout << "способ 1: ";
+    std::cout << "способ 1: ";
     for (char c : perm1) {
-        cout << c;
+        std::cout << c;
     }
 
-    cout << "\nспособ 2: ";
+    std::cout << "\nспособ 2: ";
     for (char c : perm2) {
-        cout << c;
+        std::cout << c;
     }
 
-    cout << "\n";
+    std::cout << "\n";
 
-    return 0;  
+    return 0;
 }
